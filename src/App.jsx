8aca5d5a3646c60b9326fff1b9cca03475b0891d
Pichikachandu <chandupichika0@gmail.com>
2025-06-26 23:06:02 +0530

    Fix import paths to use correct case sensitivity for Linux deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Doctors from './pages/Doctors';
-import Login from './pages/login';
+import Login from './pages/Login';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import MyProfile from './pages/myprofile';
+import MyProfile from './pages/MyProfile';
 import MyAppointments from './pages/MyAppointments';
 import Appointment from './pages/Appointment';
 import Navbar from './components/Navbar';
